ddbfd125748b5cb709aa6489eb2e3a4b577242ec
Larry Fang <lfang615@gmail.com>
2023-08-17 17:06:02 -0500

    Iterate auth/jwt.py from boilerplate

--- app/auth/jwt.py
+++ app/auth/jwt.py
@@ -1,46 +1,72 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from datetime import datetime, timedelta
+from typing import Any, Union
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from typing import Annotated
 from pydantic import BaseModel
-from models import UserInDB, User, TokenData
+
+from app.core import config
+from app.db.models import UserInDB
+from app.dependencies import get_user_repository
 
 
-SECRET_KEY = "secret"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# For hashing and verifying passwords
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# This class is used to get token data
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+# This class is used to get user data from the token
+class TokenData(BaseModel):
+    username: str = None
+
+# This is used to get the token URL (e.g., /token)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-fake_users_db = {}
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def create_access_token(data: dict):
+# For creating the access token
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     to_encode = data.copy()
-    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return token
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
+    return encoded_jwt
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+
+
+# To get the current user from the token
+async def get_current_user(token: str = Depends(oauth2_scheme), user_repository = Depends(get_user_repository)) -> Union[UserInDB, None]:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = await user_repository.get(username=token_data.username)
     if user is None:
-        raise credentials_exception
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
+
+# For verifying the password
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# For hashing the password
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+if __name__ == "__main__":
+    print(get_password_hash("password"))
