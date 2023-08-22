a13c8182a0927e4c83f1626502798bfa965109ce
Larry Fang <lfang615@gmail.com>
2023-08-21 19:39:34 -0500

    Fix unit test: test_place_order_authenticated

--- tests/test_order.py
+++ tests/test_order.py
@@ -6,12 +6,12 @@ import json
 
 @pytest.mark.asyncio
 async def test_place_order_unauthenticated(client: AsyncClient, limit_order: json):
-    response = await client.post(url="/order/place_order/", json=limit_order)  # Replace '...' with your test data
+    response = await client.post(url="/order/place_order/", json=json.dumps(limit_order))  # Replace '...' with your test data
     assert response.status_code == 401
 
 @pytest.mark.asyncio
 async def test_place_order_authenticated(client: AsyncClient, test_user_token: str, mock_get_user, limit_order: json):
     headers = {"Authorization": f"Bearer {test_user_token}"}
-    response = await client.post("/order/place_order/", json=limit_order, headers=headers)
+    response = await client.post("/order/place_order/", data=json.dumps(limit_order), headers=headers)
     print(response.json())
     assert response.status_code == 200  # Or whatever status code you expect for a successful order placement
