316954a201e72a6a59d3955e79060286ad88565c
Larry Fang <lfang615@gmail.com>
2023-09-11 18:19:18 -0500

    Start kafka producer in main.py

--- app/main.py
+++ app/main.py
@@ -2,23 +2,26 @@ from fastapi import FastAPI
 from app.api.endpoints import auth, order, position
 from app.core.logging import AsyncLogger
 from app.db.services.redisservice import AsyncRedisService
+from app.db.services.kafkaproducer import KafkaProducer
 
 app = FastAPI()
-
 
 app.include_router(auth.router, prefix="/auth", tags=["authentication"])
 app.include_router(order.router, prefix="/order", tags=["orders"])
 app.include_router(position.router, prefix="/position", tags=["positions"])
 
+
 @app.on_event("startup")
 async def startup_event():
-    logger_instance = AsyncLogger()    
+    logger_instance = AsyncLogger()
     logger_instance.info("Starting up the application...")
 
-    await AsyncRedisService().get_connection()
+    await AsyncRedisService().get_connection()    
+    await KafkaProducer().start()
     
 
 @app.on_event("shutdown")
 async def shutdown_event():
     AsyncLogger().info("Shutting down the application...")
     await AsyncRedisService.close()
+    await KafkaProducer().stop()
